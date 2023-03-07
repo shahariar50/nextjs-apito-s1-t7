8a169b8cc77788ec480ff63a4dfe005bce3129a0
shahariar50 <shahariar.touhid@gmail.com>
2023-03-08 01:05:14 +0600

    added statistics section

--- components/page/homepage/StatisticsSection.tsx
+++ components/page/homepage/StatisticsSection.tsx
@@ -1,56 +1,51 @@
 const StatisticsSection = () => {
   return (
-    <section className="pt-7 pb-0">
+    <div className="untree_co-section count-numbers py-5">
       <div className="container">
         <div className="row">
-          <div className="col-6 col-lg mb-5">
-            <div className="text-center">
-              <picture>
-                <img src="/img/icons/awards.png" alt="..." />
-              </picture>
-              <h1 className="text-primary mt-4">26+</h1>
-              <h5 className="text-800">Awards won</h5>
+          <div className="col-6 col-sm-6 col-md-6 col-lg-3">
+            <div className="counter-wrap">
+              <div className="counter">
+                <span className="" data-number="9313">
+                  0
+                </span>
+              </div>
+              <span className="caption">No. of Travels</span>
             </div>
           </div>
-          <div className="col-6 col-lg mb-5">
-            <div className="text-center">
-              <picture>
-                <img src="/img/icons/states.png" alt="..." />
-              </picture>
-              <h1 className="text-primary mt-4">65+</h1>
-              <h5 className="text-800">States covered</h5>
+          <div className="col-6 col-sm-6 col-md-6 col-lg-3">
+            <div className="counter-wrap">
+              <div className="counter">
+                <span className="" data-number="8492">
+                  0
+                </span>
+              </div>
+              <span className="caption">No. of Clients</span>
             </div>
           </div>
-          <div className="col-6 col-lg mb-5">
-            <div className="text-center">
-              <picture>
-                <img src="/img/icons/clients.png" alt="..." />
-              </picture>
-              <h1 className="text-primary mt-4">689K+</h1>
-              <h5 className="text-800">Happy clients</h5>
+          <div className="col-6 col-sm-6 col-md-6 col-lg-3">
+            <div className="counter-wrap">
+              <div className="counter">
+                <span className="" data-number="100">
+                  0
+                </span>
+              </div>
+              <span className="caption">No. of Employees</span>
             </div>
           </div>
-          <div className="col-6 col-lg mb-5">
-            <div className="text-center">
-              <picture>
-                <img src="/img/icons/goods.png" alt="..." />
-              </picture>
-              <h1 className="text-primary mt-4">130M+</h1>
-              <h5 className="text-800">Goods delivered</h5>
-            </div>
-          </div>
-          <div className="col-6 col-lg mb-5">
-            <div className="text-center">
-              <picture>
-                <img src="/img/icons/business.png" alt="..." />
-              </picture>
-              <h1 className="text-primary mt-4">130M+</h1>
-              <h5 className="text-800">Business hours</h5>
+          <div className="col-6 col-sm-6 col-md-6 col-lg-3">
+            <div className="counter-wrap">
+              <div className="counter">
+                <span className="" data-number="120">
+                  0
+                </span>
+              </div>
+              <span className="caption">No. of Countries</span>
             </div>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
