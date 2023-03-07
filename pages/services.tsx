52027936fab6c2f4e4385fb4d362568cd9d43351
shahariar50 <shahariar.touhid@gmail.com>
2023-03-08 01:25:48 +0600

    added service page

---
+++ pages/services.tsx
@@ -0,0 +1,12 @@
+import MainLayout from "components/layout/MainLayout";
+import HeroSection from "components/page/servicepage/HeroSection";
+
+const ServicesPage = () => {
+  return (
+    <MainLayout>
+      <HeroSection />
+    </MainLayout>
+  );
+};
+
+export default ServicesPage;
