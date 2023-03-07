1263479247cf663b447ee5238ccb09f33c5ddf7b
shahariar50 <shahariar.touhid@gmail.com>
2023-03-08 01:04:06 +0600

    added service section

--- components/page/homepage/ServiceSection.tsx
+++ components/page/homepage/ServiceSection.tsx
@@ -1,188 +1,82 @@
 const ServiceSection = () => {
   return (
-    <section className="py-7" id="services" container-xl="container-xl">
+    <div className="untree_co-section">
       <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-md-8 col-lg-5 text-center mb-3">
-            <h5 className="text-danger">SERVICES</h5>
-            <h2>Our services for you</h2>
+        <div className="row mb-5 justify-content-center">
+          <div className="col-lg-6 text-center">
+            <h2 className="section-title text-center mb-3">Our Services</h2>
+            <p>
+              Far far away, behind the word mountains, far from the countries
+              Vokalia and Consonantia, there live the blind texts. Separated
+              they live in Bookmarksgrove right at the coast of the Semantics, a
+              large language ocean.
+            </p>
           </div>
         </div>
-        <div className="row h-100 justify-content-center">
-          <div className="col-md-4 pt-4 px-md-2 px-lg-3">
-            <div className="card h-100 px-lg-5 card-span">
-              <div className="card-body d-flex flex-column justify-content-around">
-                <div className="text-center pt-5">
-                  <picture>
-                    <img
-                      className="img-fluid"
-                      src="/img/icons/services-1.svg"
-                      alt="..."
-                    />
-                  </picture>
-                  <h5 className="my-4">Business Services</h5>
-                </div>
-                <p>
-                  Offering home delivery around the city, where your products
-                  will be at your doorstep within 48-72 hours.
-                </p>
-                <ul className="list-unstyled">
-                  <li className="mb-2">
-                    <span className="me-2">
-                      <i
-                        className="fas fa-circle text-primary"
-                        style={{ fontSize: ".5rem" }}
-                      ></i>
-                    </span>
-                    Corporate goods
-                  </li>
-                  <li className="mb-2">
-                    <span className="me-2">
-                      <i
-                        className="fas fa-circle text-primary"
-                        style={{ fontSize: ".5rem" }}
-                      ></i>
-                    </span>
-                    Shipment
-                  </li>
-                  <li className="mb-2">
-                    <span className="me-2">
-                      <i
-                        className="fas fa-circle text-primary"
-                        style={{ fontSize: ".5rem" }}
-                      ></i>
-                    </span>
-                    Accesories
-                  </li>
-                </ul>
-                <div className="text-center my-5">
-                  <div className="d-grid">
-                    <button className="btn btn-outline-danger" type="submit">
-                      Learn more{" "}
-                    </button>
-                  </div>
-                </div>
+        <div className="row align-items-stretch">
+          <div className="col-lg-4 order-lg-1">
+            <div className="h-100">
+              <div className="frame h-100">
+                <div
+                  className="feature-img-bg h-100"
+                  style={{
+                    backgroundImage: "url('/images/hero-slider-1.jpg')",
+                  }}
+                ></div>
               </div>
             </div>
           </div>
-          <div className="col-md-4 pt-4 px-md-2 px-lg-3">
-            <div className="card h-100 px-lg-5 card-span">
-              <div className="card-body d-flex flex-column justify-content-around">
-                <div className="text-center pt-5">
-                  <picture>
-                    <img
-                      className="img-fluid"
-                      src="/img/icons/services-2.svg"
-                      alt="..."
-                    />
-                  </picture>
-                  <h5 className="my-4">Statewide Services</h5>
-                </div>
-                <p>
-                  Offering home delivery around the city, where your products
-                  will be at your doorstep within 48-72 hours.
+
+          <div className="col-6 col-sm-6 col-lg-4 feature-1-wrap d-md-flex flex-md-column order-lg-1">
+            <div className="feature-1 d-md-flex">
+              <div className="align-self-center">
+                <span className="flaticon-house display-4 text-primary"></span>
+                <h3>Beautiful Condo</h3>
+                <p className="mb-0">
+                  Even the all-powerful Pointing has no control about the blind
+                  texts.
                 </p>
-                <ul className="list-unstyled">
-                  <li className="mb-2">
-                    <span className="me-2">
-                      <i
-                        className="fas fa-circle text-primary"
-                        style={{ fontSize: ".5rem" }}
-                      ></i>
-                    </span>
-                    Unlimited Bandwidth
-                  </li>
-                  <li className="mb-2">
-                    <span className="me-2">
-                      <i
-                        className="fas fa-circle text-primary"
-                        style={{ fontSize: ".5rem" }}
-                      ></i>
-                    </span>
-                    Encrypted Connection
-                  </li>
-                  <li className="mb-2">
-                    <span className="me-2">
-                      <i
-                        className="fas fa-circle text-primary"
-                        style={{ fontSize: ".5rem" }}
-                      ></i>
-                    </span>
-                    Yes Traffic Logs
-                  </li>
-                </ul>
-                <div className="text-center my-5">
-                  <div className="d-grid">
-                    <button
-                      className="btn btn-danger hover-top btn-glow border-0"
-                      type="submit"
-                    >
-                      Learn more
-                    </button>
-                  </div>
-                </div>
+              </div>
+            </div>
+
+            <div className="feature-1 ">
+              <div className="align-self-center">
+                <span className="flaticon-restaurant display-4 text-primary"></span>
+                <h3>Restaurants & Cafe</h3>
+                <p className="mb-0">
+                  Even the all-powerful Pointing has no control about the blind
+                  texts.
+                </p>
               </div>
             </div>
           </div>
-          <div className="col-md-4 pt-4 px-md-2 px-lg-3">
-            <div className="card h-100 px-lg-5 card-span">
-              <div className="card-body d-flex flex-column justify-content-around">
-                <div className="text-center pt-5">
-                  <picture>
-                    <img
-                      className="img-fluid"
-                      src="/img/icons/services-3.svg"
-                      alt="..."
-                    />
-                  </picture>
-                  <h5 className="my-4">Personal Services</h5>
-                </div>
-                <p>
-                  You can trust us to safely deliver your most sensitive
-                  documents to the specific area in a short time.
+
+          <div className="col-6 col-sm-6 col-lg-4 feature-1-wrap d-md-flex flex-md-column order-lg-3">
+            <div className="feature-1 d-md-flex">
+              <div className="align-self-center">
+                <span className="flaticon-mail display-4 text-primary"></span>
+                <h3>Easy to Connect</h3>
+                <p className="mb-0">
+                  Even the all-powerful Pointing has no control about the blind
+                  texts.
                 </p>
-                <ul className="list-unstyled">
-                  <li className="mb-2">
-                    <span className="me-2">
-                      <i
-                        className="fas fa-circle text-primary"
-                        style={{ fontSize: ".5rem" }}
-                      ></i>
-                    </span>
-                    Unlimited Bandwidth
-                  </li>
-                  <li className="mb-2">
-                    <span className="me-2">
-                      <i
-                        className="fas fa-circle text-primary"
-                        style={{ fontSize: ".5rem" }}
-                      ></i>
-                    </span>
-                    Encrypted Connection
-                  </li>
-                  <li className="mb-2">
-                    <span className="me-2">
-                      <i
-                        className="fas fa-circle text-primary"
-                        style={{ fontSize: ".5rem" }}
-                      ></i>
-                    </span>
-                    Yes Traffic Logs
-                  </li>
-                </ul>
-                <div className="text-center my-5">
-                  <div className="d-grid">
-                    <button className="btn btn-outline-danger" type="submit">
-                      Learn more{" "}
-                    </button>
-                  </div>
-                </div>
+              </div>
+            </div>
+
+            <div className="feature-1 d-md-flex">
+              <div className="align-self-center">
+                <span className="flaticon-phone-call display-4 text-primary"></span>
+                <h3>24/7 Support</h3>
+                <p className="mb-0">
+                  Even the all-powerful Pointing has no control about the blind
+                  texts.
+                </p>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
